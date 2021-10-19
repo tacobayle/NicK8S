6df15eacbddf34a3cdc2957310f0b882e59b7b57
tacobayle <nicolas.bayle@gmail.com>
2021-10-19 18:44:30 +0000

    20211910_K8s_auto_complete

--- output.tf
+++ output.tf
@@ -1,6 +1,6 @@
 # Outputs for Terraform
 
-output "master" {
+output "master_K8s" {
   value = vsphere_virtual_machine.master.*.default_ip_address
 }
 
@@ -33,3 +33,4 @@ output "destroy" {
 output "ako_install" {
   value = "helm --debug install ako/ako --generate-name --version ${var.vmw.kubernetes.clusters[0].ako.version} -f values.yml --namespace=${var.vmw.kubernetes.clusters[0].ako.namespace} --set avicredentials.username=admin --set avicredentials.password=$avi_password"
 }
+
